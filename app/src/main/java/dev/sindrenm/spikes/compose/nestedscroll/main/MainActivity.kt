cd3c44ab69471d4381d13aec4b564d06d89b0a74
Sindre Moen <sindrenm@gmail.com>
2021-03-16 19:56:53 +0100

    Set up Android Studio project

---
+++ app/src/main/java/dev/sindrenm/spikes/compose/nestedscroll/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.sindrenm.spikes.compose.nestedscroll.main
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                MainScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/dev/sindrenm/spikes/compose/nestedscroll/main/MainScreen.kt
@@ -0,0 +1,7 @@
+package dev.sindrenm.spikes.compose.nestedscroll.main
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun MainScreen() {
+}
