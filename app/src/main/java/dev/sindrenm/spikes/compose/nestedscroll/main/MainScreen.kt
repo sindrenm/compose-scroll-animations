c028b056ca063dc20ee78bfd8d07245e8bdbed8c
Sindre Moen <sindrenm@gmail.com>
2021-03-16 23:17:51 +0100

    Animate avatar and name based on scroll position

--- app/src/main/java/dev/sindrenm/spikes/compose/nestedscroll/main/MainScreen.kt
+++ app/src/main/java/dev/sindrenm/spikes/compose/nestedscroll/main/MainScreen.kt
@@ -1,5 +1,8 @@
 package dev.sindrenm.spikes.compose.nestedscroll.main
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -10,6 +13,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
@@ -24,33 +29,60 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 internal fun MainScreen() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            item { Header() }
+    val listState = rememberLazyListState()
 
-            items(100) {
-                ListItem(
-                    text = { Text("List item #$it") }
-                )
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Box {
+            LazyColumn(state = listState) {
+                stickyHeader {
+                    Header(listState = listState)
+                }
+
+                items(100) {
+                    ListItem(text = { Text("List item #$it") })
+                }
             }
+
+            Text(
+                text = """
+                    First visible index: ${listState.firstVisibleItemIndex}
+                    First visible offset: ${listState.firstVisibleItemScrollOffset}
+                """.trimIndent(),
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp),
+            )
         }
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-private fun Header() {
+private fun Header(listState: LazyListState) {
+    val coroutineScope = rememberCoroutineScope()
+
+    val showStuff = derivedStateOf {
+        listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0
+    }.value
+
+    val alpha = animateFloatAsState(targetValue = if (showStuff) 1f else 0f).value
+
     Column {
         Box {
             CurvedBackground()
@@ -60,7 +92,7 @@ private fun Header() {
                     modifier = Modifier
                         .align(Alignment.TopEnd)
                         .padding(top = 8.dp, end = 20.dp),
-                    onClick = {},
+                    onClick = { coroutineScope.launch { listState.animateScrollToItem(0) } },
                 ) {
                     Icon(Icons.Default.Menu, contentDescription = null)
                 }
@@ -71,7 +103,8 @@ private fun Header() {
                     .padding(top = AvatarTopPadding)
                     .size(AvatarSize)
                     .clip(CircleShape)
-                    .align(Alignment.TopCenter),
+                    .align(Alignment.TopCenter)
+                    .graphicsLayer(alpha = alpha),
                 contentAlignment = Alignment.Center,
             ) {
                 Image(
@@ -90,6 +123,7 @@ private fun Header() {
             style = MaterialTheme.typography.h5,
             modifier = Modifier
                 .padding(top = 8.dp, bottom = 20.dp)
+                .graphicsLayer(alpha = alpha)
                 .align(Alignment.CenterHorizontally),
         )
     }
